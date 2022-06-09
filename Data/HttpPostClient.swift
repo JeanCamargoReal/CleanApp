29f7c3cefa06bcd2a07099ea2df18be66832dec8
JeanCamargoReal <jeancamargoreal@icloud.com>
2022-06-09 16:31:34 -0300

    Move production code to their own files

---
+++ Data/HttpPostClient.swift
@@ -0,0 +1,12 @@
+//
+//  HttpPostClient.swift
+//  Data
+//
+//  Created by Jean Camargo on 09/06/22.
+//
+
+import Foundation
+
+public protocol HttpPostClient {
+	func post(to url: URL, with data: Data?)
+}
---
+++ Data/RemoteAddAccount.swift
@@ -0,0 +1,23 @@
+//
+//  RemoteAddAccount.swift
+//  Data
+//
+//  Created by Jean Camargo on 09/06/22.
+//
+
+import Foundation
+import Domain
+
+public final class RemoteAddAccount {
+	private let url: URL
+	private let httpClient: HttpPostClient
+	
+	public init(url: URL, httpClient: HttpPostClient) {
+		self.url = url
+		self.httpClient = httpClient
+	}
+	
+	public func add(addAccountModel: AddAccountModel) {
+		httpClient.post(to: url, with: addAccountModel.toData())
+	}
+}
