858158adbe57c317ad6d8167ace4e570ec6fca29
JeanCamargoReal <jeancamargoreal@icloud.com>
2022-06-10 16:15:25 -0300

    Add file and line in XCTAsserts

--- DataTests/UsesCases/RemoteAddAccountTests.swift
+++ DataTests/UsesCases/RemoteAddAccountTests.swift
@@ -9,8 +9,7 @@ import XCTest
 import Domain
 import Data
 
-// -------------------------------------------------------------------
-
+// MARK: - Tests
 class RemoteAddAccountTests: XCTestCase {
 	func test_add_should_call_httpClient_with_correct_url() {
 		let url = makeUrl()
@@ -55,6 +54,7 @@ class RemoteAddAccountTests: XCTestCase {
 	}
 }
 
+// MARK: - Extension
 extension RemoteAddAccountTests {
 	func makeSut(url: URL = URL(string: "http://any-url.com")!) -> (sut: RemoteAddAccount, httpClientSpy: HttpClientSpy) {
 		let httpClientSpy = HttpClientSpy()
@@ -63,17 +63,17 @@ extension RemoteAddAccountTests {
 		return (sut, httpClientSpy)
 	}
 	
-	func expect(_ sut: RemoteAddAccount, completeWith expectedResult: Result<AccountModel, DomainError>, when action: () -> Void) {
+	func expect(_ sut: RemoteAddAccount, completeWith expectedResult: Result<AccountModel, DomainError>, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
 		let exp = expectation(description: "waiting")
 		
 		sut.add(addAccountModel: makeAddAccountModel()) { receivedResult in
 			switch (expectedResult, receivedResult) {
 				case (.failure(let expectedError), .failure(let receivedError)):
-					XCTAssertEqual(expectedError, receivedError)
+					XCTAssertEqual(expectedError, receivedError, file: file, line: line)
 				case (.success(let expectedAccount), .success(let receivedAccount)):
-					XCTAssertEqual(expectedAccount, receivedAccount)
+					XCTAssertEqual(expectedAccount, receivedAccount, file: file, line: line)
 				default:
-					XCTFail("Expected \(expectedResult) received \(receivedResult) instead")
+					XCTFail("Expected \(expectedResult) received \(receivedResult) instead", file: file, line: line)
 			}
 			exp.fulfill()
 		}
