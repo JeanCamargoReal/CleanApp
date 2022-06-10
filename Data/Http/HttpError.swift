826807481c0872bca49d3365ca8ee5f7ba2a18e9
JeanCamargoReal <jeancamargoreal@icloud.com>
2022-06-10 12:16:04 -0300

    Ensure RemoteAddAccount completes with error when HttpClient completes with error

---
+++ Data/Http/HttpError.swift
@@ -0,0 +1,12 @@
+//
+//  HttpError.swift
+//  Data
+//
+//  Created by Jean Camargo on 10/06/22.
+//
+
+import Foundation
+
+public enum HttpError: Error {
+	case noConnectivity
+}
---
+++ Domain/Errors/DomainError.swift
@@ -0,0 +1,12 @@
+//
+//  DomainError.swift
+//  Domain
+//
+//  Created by Jean Camargo on 10/06/22.
+//
+
+import Foundation
+
+public enum DomainError: Error {
+	case unexpected
+}
