b08a2a2f2d7a26457915db1c782455107e42217e
JeanCamargoReal <jeancamargoreal@icloud.com>
2022-06-13 14:08:53 -0300

    Add Infra layer

---
+++ InfraTests/AlamofireAdapterTests.swift
@@ -0,0 +1,14 @@
+//
+//  AlamofireAdapterTests.swift
+//  AlamofireAdapterTests
+//
+//  Created by Jean Camargo on 13/06/22.
+//
+
+import XCTest
+
+class AlamofireAdapterTests: XCTestCase {
+	func test_() {
+		
+	}
+}
